fba70fd110197156a4e032091c8328259a7b3a3d
macmini_m1_2022 <sakaguchi-daimon-nh@ynu.jp>
2024-03-13 13:34:43 +0900

    ver20240313

--- sphere_model_lib/sphere_parameter.py
+++ sphere_model_lib/sphere_parameter.py
@@ -10,13 +10,13 @@ for r_ in r:
         for t_ in t:
             l.append([r_, d_, t_])
 print(len(r), len(t), len(l))
-dfp = pd.DataFrame(l, columns=['r', 'd', "θ"])
+dfp = pd.DataFrame(l, columns=['R', 'd', "θ"])
 
 dfp = dfp.assign(x=dfp["d"] * np.cos(np.radians(dfp["θ"])),
                  y=0,
                  z=dfp["d"] * np.sin(np.radians(dfp["θ"])))
 dfp = dfp.round(decimals={
-    'r': 1, 'd': 1, 'θ': 0,
+    'R': 1, 'd': 1, 'θ': 0,
     "x": 5, "y": 5, "z": 5})
 print(dfp)
 dfp.to_csv("../sphere_parameter/sphere_parameter.csv", index=False)
